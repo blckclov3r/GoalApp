d163aef1b51c36d013412917420c4dde198965dd
blckclov3r <blckclov3r@gmail.com>
2022-07-19 14:18:31 +0800

    deploy

--- backend/server.js
+++ backend/server.js
@@ -1,3 +1,5 @@
+const path = require('path');
+
 const express = require('express');
 const colors = require('colors')
 const dotenv = require('dotenv').config();
@@ -15,8 +17,18 @@ const app = express();
 app.use(express.json())
 app.use(express.urlencoded({extended: false}))
 
-app.use('/api/goals',require('./routes/goalRoutes'))
-app.use('/api/users',require('./routes/userRoutes'))
+app.use('/api/goals',require('./routes/goalRoutes'));
+app.use('/api/users',require('./routes/userRoutes'));
+
+// serve frontend
+if(process.env.NODE_ENV === 'production'){
+    app.use(express.static(path.join(__dirname,'../frontend/build')))
+    app.get('*',(req,res)=>{
+        return res.sendFile(    path.resolve(__dirname, '../', 'frontend', 'build', 'index.html'))
+    })
+}else{
+    app.get('/',(req,res)=>res.send('Please set to production'))
+}
 
  app.use(errorHandler);
 
